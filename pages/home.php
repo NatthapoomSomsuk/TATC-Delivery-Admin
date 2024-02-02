64bc7f48ae7ac5cd99fe9f9d4f6bccd4647a498a
Dragon Style Studio <natthapoom.somsuk@gmail.com>
2024-02-02 09:19:52 +0700

    admin login

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,14 @@
-<div class=" d-flex flex-column vh-100">
-    <div class=" flex-shrink-1 h-100 ">
-        <div class=" w-100 h-100 d-flex flex-column justify-content-center">
-            asd
+<div class=" d-flex flex-column  vh-100">
+    <div class=" flex-shrink-1 h-100 overflow-hidden d-flex flex-column">
+        <div class=" d-flex justify-content-between px-4 pt-3">
+            <p class=" text-center fs-2 m-0">เมนูการจัดการขอผู้ดูแล</p>
+        </div>
+        <div class=" h-100 overflow-scroll ">
+         
+
+
+
         </div>
     </div>
+    <?php include('./components/nva_emp_buttom.php') ?>
 </div>
